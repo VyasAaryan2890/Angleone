30d3d756edf28762df87c4ed68fe242ee4a5586c
vyasaarayn <vyas12309@gmail.com>
2024-12-09 20:55:48 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Login from './Login/Login';
 import Start from './Start/Start';
 
-
 function App() {
-    return (
-        
-            <Routes>
-                <Route path="/" element={<Start/>} />
-                <Route path="/login" element={<Login/>} />
-            </Routes>
-    
-    );
+  
+  return (
+      <Routes>
+        <Route path="/" element={<Start />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+  );
 }
 
 export default App;
