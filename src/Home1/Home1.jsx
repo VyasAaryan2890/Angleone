3750dfc04dd921619867f27c46d0e9340a6b02fa
vyasaarayn <vyas12309@gmail.com>
2024-12-10 22:37:40 +0530

    g

--- src/Home1/Home1.jsx
+++ src/Home1/Home1.jsx
@@ -1,11 +1,80 @@
-import React from 'react'
-import './Home1.css';
-const Home1 = () => {
-  return (
-    <div>
-      <h1>Welcome To AngleOne Home</h1>
-    </div>
-  )
-}
+import React, { useState } from "react";
+import "./Home1.css";
+import alert from "../assets/alarm.png";
+import notification from "../assets/notification.png";
+import search from "../assets/search.png";
+import home from "../assets/home.png";
+import watch from "../assets/bookmark.png";
+import bag from "../assets/bag.png";
+import file from "../assets/file.png";
+import acc from "../assets/account.png";
 
-export default Home1
+const Home1 = () => {
+    const [activeTab, setActiveTab] = useState("Stocks"); // Track active tab for navParent
+    const [activeIcon, setActiveIcon] = useState("Home"); // Track active icon for navLast
+
+    const handleTabClick = (tabName) => {
+        setActiveTab(tabName);
+    };
+
+    const handleIconClick = (iconName) => {
+        setActiveIcon(iconName);
+    };
+
+    return (
+        <div>
+            <div className="Navigation">
+                <div className="navParent">
+                    {/* code for navChild */}
+                    <div className="navChild">
+                        <div>
+                            <h1>Home</h1>
+                            <h2>News</h2>
+                        </div>
+
+                        <ul>
+                            <img src={alert} alt="alert" />
+                            <img src={notification} alt="notification" />
+                            <img src={search} alt="search" />
+                        </ul>
+                    </div>
+
+                    {/* code for nav parent */}
+                    <ul className="ul">
+                        {["Stocks", "F&O", "Mutual Funds", "FD"].map((tabName) => (
+                            <li
+                                key={tabName}
+                                className={activeTab === tabName ? "active" : ""}
+                                onClick={() => handleTabClick(tabName)}
+                            >
+                                {tabName}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                {/* Bottom Navigation */}
+                <div className="navLast">
+                    {[
+                        { name: "Home", icon: home },
+                        { name: "Watchlist", icon: watch },
+                        { name: "Portfolio", icon: bag },
+                        { name: "Orders", icon: file },
+                        { name: "Account", icon: acc },
+                    ].map(({ name, icon }) => (
+                        <div
+                            key={name}
+                            className={`iocn ${activeIcon === name ? "activeIcon" : ""}`}
+                            onClick={() => handleIconClick(name)}
+                        >
+                            <img src={icon} alt={name} />
+                            <p>{name.toUpperCase()}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home1;
